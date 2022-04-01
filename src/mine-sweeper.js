afe696f8a9dc306c5208ba8e20c1999921b19676
m208 <avcvet@yandex.ru>
2022-04-01 20:58:33 +0300

    feat: Minesweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const h = matrix.length;
+  const w = matrix[0].length;
+
+  const result = JSON.parse(JSON.stringify(matrix)); //deep clone
+
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+
+      //console.log('cell y: ' + y + ' x: ' + x);
+
+      let bombsCount = 0;
+      for (let ys = y - 1; ys <= y + 1; ys++) {
+        for (let xs = x - 1; xs <= x + 1; xs++) {
+
+          if ((xs >= 0 && xs < w) && (ys >= 0 && ys < h)) {
+            if (xs == x && ys == y) { }  //center cell, skip it
+            else {
+              bombsCount += matrix[ys][xs];
+              //console.log('ys: ' + ys, 'xs: ' + xs, 'val: ' + matrix[ys][xs], 'bombsCount: ', bombsCount);
+            }
+            result[y][x] = bombsCount;
+          }
+
+        }
+
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
