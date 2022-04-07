d1957943415f5c5450167e535869d51d17401e60
m208 <avcvet@yandex.ru>
2022-04-08 00:57:08 +0300

    feat: Vigenire cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor(direct = true) {
+    this.directType = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, keyWord) {
+    if (message && keyWord)
+      return this.doCryptJob(message, keyWord, 'encrypt');
+    throw new Error('Incorrect arguments!');
+  }
+
+  decrypt(message, keyWord) {
+    if (message && keyWord)
+      return this.doCryptJob(message, keyWord, 'decrypt');
+    throw new Error('Incorrect arguments!');
+  }
+
+  doCryptJob(message, keyWord, job) {
+
+    const keyPhrase = this.getPassPhrase(keyWord, message);
+    let resultMessage = '';
+
+    for (let i = 0; i < message.length; i++) {
+      resultMessage += this.getCryptoChar(message[i], keyPhrase[i], job);
+    }
+
+    return (this.directType) ? resultMessage : resultMessage.split('').reverse().join('');
+  }
+
+  getCryptoChar(text, key, job) {
+
+    const keyIndex = this.alphabet.indexOf(key.toUpperCase());
+    const textIndex = this.alphabet.indexOf(text.toUpperCase());
+
+    if (textIndex == -1) return text;
+
+    let index;
+    if (job === 'encrypt') index = keyIndex + textIndex;
+    else if (job === 'decrypt') index = 26 - keyIndex + textIndex;
+
+    if (index >= 26) index = index - 26;
+
+    return this.alphabet[index];
+  }
+
+  getPassPhrase(keyWord, message) {
+
+    let keyPhrase = keyWord;
+
+    while (keyPhrase.length < message.length) {
+      keyPhrase = keyPhrase.repeat(2);
+    }
+    keyPhrase = keyPhrase.split('');
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.alphabet.includes(message[i].toUpperCase())) {
+        keyPhrase.splice(i, 0, ' ');
+      }
+    }
+
+    keyPhrase.length = message.length;
+
+    return keyPhrase.join('');
   }
 }
 
