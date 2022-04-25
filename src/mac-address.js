d94df3ae63b3f8d864a1f4160daf961d044c898f
m208 <avcvet@yandex.ru>
2022-04-26 01:38:56 +0300

    feat: Mac addres task

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+
+  const isHex = (str) => {
+    return !isNaN(parseInt(str, 16));
+  }
+
+  const addres = n.split('-');
+  let answer = true;
+
+  for (let a of addres) {
+    if (!isHex(a)) answer = false;
+  }
+
+  return answer;
+
 }
 module.exports = {
   isMAC48Address
